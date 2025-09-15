5effbd717581ea7583089089d43a8f3e535d0861
Vadim Popov <vadimpopov10102005@gmail.com>
2025-09-15 20:38:11 +0700

    Добавил WindowsForms в общий проект

--- WinFormsApp2/WinFormsApp2/obj/Debug/net8.0-windows/WinFormsApp2.AssemblyInfo.cs
+++ WinFormsApp2/WinFormsApp2/obj/Debug/net8.0-windows/WinFormsApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19f44f2de648c27fac6cc1e24049db18ee1685c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
