7f9ed10f3fdc0b59fc66fb5d991a2724c4eb0668
Vadim Popov <vadimpopov10102005@gmail.com>
2025-09-16 11:36:44 +0700

    Создание Domain файла с общей бизнес-логикой

---
+++ Domain/Domain/Models.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Models;
+
+public sealed class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public string Author { get; set; } = "";
+    public string Genre { get; set; } = "";
+    public int Year { get; set; }
+    public int Pages { get; set; }
+    public bool IsRead { get; set; }
+
+    public override string ToString() => $"{Title} — {Author} ({Year}), {Genre}";
+}
